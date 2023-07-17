6c081438c8c9551d8847f70f31c4086f435c77af
aimen114 <aimenzafar095@gmail.com>
2023-07-17 22:17:58 +0500

    added HTML structure
    added basic html structure and icons

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-alert("hello world");
+// Get the necessary elements
+const cityName = document.getElementById("cityName");
+const searchIcon = document.getElementById("searchIcon");
+const searchInput = document.getElementById("searchInput");
+
+// Toggle the visibility of the search input when the icon is clicked
+searchIcon.addEventListener("click", () => {
+  searchInput.style.display = "inline-block";
+  searchInput.focus();
+});
+
+// Update the city name and hide the input when a city is searched
+searchInput.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    cityName.textContent = searchInput.value;
+    searchInput.value = "";
+    searchInput.style.display = "none";
+  }
+});
