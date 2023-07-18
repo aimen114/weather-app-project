0ee188aad446b2715b3bb9ee40a8554afa0e4173
aimen114 <aimenzafar095@gmail.com>
2023-07-18 20:47:37 +0500

    added weather api
    added weather api and functions to display and format date and weather info

--- src/app.js
+++ src/app.js
@@ -1,20 +1,57 @@
-let cityName = document.getElementById("cityName");
+let inputGroup = document.getElementById("input-group");
+inputGroup.style.display = "none";
 
-let searchIcon = document.getElementById("searchIcon");
-let searchBar = document.getElementById("searchInput");
-searchBar.style.display = "none";
-
-function showSearchBar() {
-  searchBar.style.display = "inline-block";
-  searchBar.focus();
-}
-function updateCityName(event) {
-  if (event.key === `Enter`) {
-    cityName.textContent = searchInput.value;
-    searchInput.value = "";
-    searchBar.style.display = "none";
+function toggleSearchBarVisibility() {
+  if (inputGroup.style.display === "none") {
+    inputGroup.style.display = "block";
+  } else {
+    inputGroup.style.display = "none";
   }
 }
 
-searchIcon.addEventListener("click", showSearchBar);
-searchBar.addEventListener("keyup", updateCityName);
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes} `;
+}
+
+function displayWeatherInfo(response) {
+  console.log(response);
+  console.log(response.data.time);
+  let temp = document.getElementById("tempOfTheDay");
+  let cityInput = document.getElementById("cityName");
+  let humidity = document.getElementById("humidityNow");
+  let wind = document.getElementById("windNow");
+  let weatherDisc = document.getElementById("weatherCondition");
+  let date = document.getElementById("date");
+
+  temp.innerHTML = Math.round(response.data.temperature.current);
+  cityInput.innerHTML = response.data.city;
+  humidity.innerHTML = Math.round(response.data.temperature.humidity);
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  weatherDisc.innerHTML = response.data.condition.description;
+  date.innerHTML = formatDate(response.data.time * 1000);
+}
+
+let apiKey = "95aba4bfo096ef39t52469746eae7704";
+let searchedCity = "Paris";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchedCity}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeatherInfo);
